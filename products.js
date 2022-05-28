44d294587cf60ae82dc6deb1e0f27a20f145848d
jc4milo <jc4milo311@gmail.com>
2022-05-28 18:13:03 -0500

    feat: se agrego carrito

---
+++ products.js
@@ -0,0 +1,9 @@
+const phones = [
+    {id:"phone-01",  product: 'Apple',   price: 4000000,  image: '/Images/iphone.jpg'}, 
+    {id:"phone-02",  product: 'Huawei',  price: 3500000,  image: '/Images/huawei.jpg'},
+    {id:"phone-03",  product: 'Samsung', price: 5350000,  image: '/Images/samsung.jpg'},
+    {id:"phone-04",  product: 'Xiaomi',  price: 700000,   image: '/Images/xiaomi.jpg'},
+    {id:"phone-05",  product: 'Poco',    price: 1750000,  image: '/Images/poco.jpg'},
+    {id:"phone-06",  product: 'Realme',  price: 800000,   image: '/Images/realme.jpg'},
+    {id:"phone-07",  product: 'hyundai', price: 230400,   image: '/Images/hyundai.jpg'},
+  ];
